e3dc1e2a4dc64778f98a67f1729fb37d1782c52c
PatoMo <73911080+PatoMo@users.noreply.github.com>
2020-11-23 19:17:25 -0300

    Update Tarea.cpp
    Modifique el guardar el archivo(agregue librerias), deje varios comentarios y creo que deberiamos poner la info del paciente dentro del loop de do-while, además al guardar la info pone la hora y fecha del momento en el que se ingreso el medicamento.

--- Tarea.cpp
+++ Tarea.cpp
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <string.h> //libreria para comparar palabras
-
-float dosis;
+#include <cstdlib>
+#include <iostream> //contiene los algoritmos basicos
+#include <fstream>  //permite la manipulacion de archivos desde el programar, tanto leer como escribir en ellos
+#include <conio.h>      // para almacenar archivos o leer archivos Externos
+#include <ctime>  //PARA LA FECHA ACTUAL
+using namespace std;
+float dosis,concentracion;
 char name[60],rut[20],confirmar[10];
 int edad,opcion,cantidadmg,cantidadml,peso;
-int main(){ /*
+
+int main(){
+   ofstream entrada;                      //ESTO ES PARA GUARDAR EN EL ARCHIVO DE NOMBRE entrada Con la hora y fecha 
+    entrada.open("entrada.txt",ios::out);
+    time_t hora = time(0);
+    char* fecha = ctime(&hora);
+   
+   //INGRESO PACIENTE           (Creo que debería ir en el loop pero no estoy seguro)
 printf("Ingrese nombre del paciente: \n");
 gets(name); //lee la cadena de caracteres y las almacena en name
 printf("Ingrese rut del paciente y edad: \n");
 scanf("%s %d",rut,&edad);
 printf("Ingrese peso del paciente en kilogramos: ");
 scanf("%d",&peso);
-    */
+
 do //Las opciones
 {   printf("¿Qué medicamento desea dosificar? \n");
     printf( " 1.  Noradrenalina \n");
@@ -30,21 +42,25 @@ do //Las opciones
         printf("Introduzca la opción(1-12) y pulse enter: ");
         scanf("%d",&opcion);
         }
-    while (opcion <1 || opcion>12); //CODIGO 1234 PARA SACAR LOOP
+    while ((opcion <1) && (opcion>12) && (opcion!=1234)); //CODIGO 1234 PARA SACAR LOOP
     switch ( opcion ){
 case 1:     do{
             printf( "\n   Concentración(mg)/Dilusión(ml) de Adrenalina : " ); //ADRENALINA
             scanf( "%d %d", &cantidadmg,&cantidadml ); //hacer lo que haya que hacer
-          float division= (cantidadmg/cantidadml);
-            printf( "Su dosis es %.4f mg/ml\n",division );
+            concentracion= (cantidadmg/cantidadml);
+            printf( "Su dosis es %.4f mg/ml\n",concentracion );
             printf("Agregue la dosis: (gamma*kilogramo*minuto) \n");
             scanf("%f",&dosis);
             printf("La dosis es %.4f, ¿confirmar?   \n",dosis); //CONFIRMAR CON PALABRA???
         do{printf("Aceptar o Cancelar \n"); //Hice un Loop para darle a aceptar o cancelar y al presionar cancelar deberia empezar 
             scanf("%s",confirmar);}
-            while(strcmp(confirmar,"aceptar") !=0 || strcmp(confirmar,"cancelar") !=0);
+            while((strcmp(confirmar,"aceptar") !=0) && (strcmp(confirmar,"cancelar") !=0));
 }
             while(strcmp(confirmar, "aceptar") != 0);
+            entrada<<"Su concentracion de adrenalina fue de "<<concentracion<<" mg/ml con una dosis de "<<dosis<<"  Fecha:"<<fecha<<endl; //GUARDADO LO QUE SE HIZO
+            entrada.close();
+            cout << "Los datos fueron almacenados correctamente \n"    ;
+            getch();
             break;
 
 case 2: printf( "\n   Dosificar Amiodarona: ");
